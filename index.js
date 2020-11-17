2dc1e2bb85342d1987ebc9809877afb1d220224e
Ye Shao <yeshao@localhost.YeShao-CentOS8>
2020-11-17 23:39:57 +0800

    feat(创建仓库): 在中国标准时间2020年10月14日写了情书
    在中国标准时间2020年10月14日写了情书，给lsy的，，，

---
+++ index.js
@@ -0,0 +1,20 @@
+function thanks(){
+window.alert("首先，我要谢谢娘子您。\n谢谢您一直以来的陪伴。\n\n感谢以下：\n"+"WPS,Mousepad,Ubunbu,腾讯云,宝塔面板,叶少");
+}
+function loghello(){
+console.log('致lsy：');
+console.log('我是你相公。');
+console.log('娘子，我爱你。');
+console.log('祝我们9999999999999');
+}
+function copyright(){
+window.alert('版权所有 ©2020 叶少&lsy。保留一切权利。\n\n\n依据《叶少代码公开条例》的相关规定开放源代码。');
+}
+function gotosrc(){
+window.location.href="./indexsource.html";
+}
+function getnowtime(){
+var nowtime = new Date();
+document.getElementById("time").innerHTML=nowtime;
+}
+
